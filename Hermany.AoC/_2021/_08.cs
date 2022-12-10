09681847f52d67338a3c9237b69af77a8777bc51
Paul Hermany <PHermany@rightpoint.com>
2022-12-10 08:12:10 -0600

    Completed 2022 Day 10

--- Hermany.AoC/_2021/_08.cs
+++ Hermany.AoC/_2021/_08.cs
@@ -1,16 +1,25 @@
+using System;
+using System.Linq;
+
 namespace Hermany.AoC._2021
 {
     public class _08 : ISolution
     {
-        public string P1Assertion { get => string.Empty; }
-        public string P2Assertion { get => string.Empty; }
+        public string P1Assertion { get => "26"; }
+        public string P2Assertion { get => "5353"; }
         public string P1(string[] input)
         {
-            throw new System.NotImplementedException();
+            return input
+                .Select(_ => 
+                    _.Split(" | ")[1]
+                    .Split(' ')
+                    .Count(s => s.Length == 2 || s.Length == 4 || s.Length == 3 || s.Length == 7)
+                )
+                .Sum().ToString();
         }
         public string P2(string[] input)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
