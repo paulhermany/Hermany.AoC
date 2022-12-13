4e8d4e27ef9068567500d4653bc31af4a2518bf9
Paul Hermany <PHermany@rightpoint.com>
2022-12-12 19:29:41 -0600

    Begin porting graph traversal common code from ssartell

---
+++ Hermany.AoC/GraphTraversal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hermany.AoC
+{
+    public class GraphTraversal
+    {
+
+        //public static TValue Dijkstra<TValue, TKey, TCompare>(TValue start, Func<TValue, bool> isEnd, Func<TValue, IEnumerable<TValue>> getNeighbors, Func<TValue, TCompare> getCost, Func<TValue, TKey> getKey)
+        //{
+        //    var notVisited = new PriorityQueue<TValue>();
+
+        //    var d = new SortedDictionary<TValue, TKey>();
+
+
+        //}
+
+        //public static TValue FindPath<TValue, TKey>(Action<TValue> push, Func<TValue> pop, Func<TValue> peek, TValue start, Func<TValue, bool> isEnd, Func<TValue, IEnumerable<TValue>> getNeighbors, Func<TValue, TKey> getKey)
+        //{
+        //    push(start);
+        //    var seen = new HashSet<TKey>();
+        //    while (null != peek())
+        //    {
+        //        var current = pop();
+        //        var key = getKey(current);
+        //        if (seen.Contains(key)) continue;
+        //        seen.Add(key);
+        //        if (isEnd(current)) return current;
+        //        foreach (var neighbor in getNeighbors(current))
+        //            push(neighbor);
+        //    }
+        //    return default(TValue);
+        //}
+    }
+}
